eaedc19c06b062716f1393b17cf02b0f8420d7ec
sheng4893166@gmail.com <samirchen@samirchen-pc.(none)>
2014-06-30 23:16:13 +0800

    a

--- tiff_util/tiff_util.c
+++ tiff_util/tiff_util.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include "../common/dieWithError.h"
 #include "../common/cpuUsage.h"
-using namespace std;
-
+#include <string.h>
 
 /* ######################## Method Declare ######################## */
 // ================= Out of this file. ================
@@ -24,6 +23,29 @@ float calThreadCPUUse(ProcStat* ps1, ProcPidStat* pps1, ProcStat* ps2, ProcPidSt
 
 int main() {
 	printf("Hello\n");
+
+	const char* fileName = "out.txt";
+	FILE* fp = fopen(fileName, "wb");
+
+	int i = 10;
+	char* str = "test";
+	size_t len = strlen(str);
+
+	fwrite(&i, sizeof(int), 1, fp);
+	fwrite(str, sizeof(char), len, fp);
+
+	fclose(fp);
+
+	fp = fopen(fileName, "rb");
+	int k = 0;
+	char buf[1024];
+	bzero(buf, 1024);
+
+	fread(&k, sizeof(int), 1, fp);
+	printf("%d\n", k);
+	fread(buf, sizeof(char), 1024, fp);
+	printf("%s\n", buf);
+
 
 	return 0;
 }
